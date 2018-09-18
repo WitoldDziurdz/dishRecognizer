2ea5fe43aaee1bb427d29f273e67e11390f423c5
RealRyuu <luki885@gmail.com>
2018-09-18 18:24:46 +0200

    Update download_datasets.py

--- src/tools/download_datasets.py
+++ src/tools/download_datasets.py
@@ -2,7 +2,8 @@ import wget
 
 caltech_256_url = 'http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar'
 food_11k_url = 'http://grebvm2.epfl.ch/lin/food/Food-11.zip'
+food-101_url = 'http://data.vision.ee.ethz.ch/cvl/food-101.tar.gz'
 
 wget.download(caltech_256_url)
 wget.download(food_11k_url)
-
+wget.download(food-101_url)
