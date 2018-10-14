070682f2ad415da972d91a90fc3617e888fa2eb8
Witold.Dzyurdz <witolddzyurdz@gmail.com>
2018-10-14 16:09:36 +0300

    add 101 script

---
+++ script_101food_copy.py
@@ -0,0 +1,64 @@
+import os, shutil
+from random import shuffle
+
+def create_dir(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+def main():
+    original_dataset_dir = 'C:\\food-101\\images'
+
+    base_dir = 'C:\\data\\101food'
+    create_dir(base_dir)
+    train_dir = os.path.join(base_dir, 'training')
+    create_dir(train_dir)
+    validation_dir = os.path.join(base_dir, 'validation')
+    create_dir(validation_dir)
+    test_dir = os.path.join(base_dir, 'evaluation')
+    create_dir(test_dir)
+
+    list_origin_dirs = []
+
+    list_train_dirs = []
+    list_validation_dirs = []
+    list_test_dirs = []
+
+    #create dirs of elements
+    for dir in os.listdir(original_dataset_dir):
+
+        origin_dir_element = os.path.join(original_dataset_dir, dir)
+        list_origin_dirs.append(origin_dir_element)
+
+        train_dir_element = os.path.join(train_dir, dir)
+        list_train_dirs.append(train_dir_element)
+        create_dir(train_dir_element)
+
+        validation_dir_element = os.path.join(validation_dir, dir)
+        list_validation_dirs.append(validation_dir_element)
+        create_dir(validation_dir_element)
+
+        test_dir_element = os.path.join(test_dir, dir)
+        list_test_dirs.append(test_dir_element)
+        create_dir(test_dir_element)
+
+
+    #copy elements from origin dirs to train, validation and test dirs
+    for i, path in enumerate(list_origin_dirs):
+        files = os.listdir(path)
+        shuffle(files)
+        print(str(i) + " of " + str(len(list_origin_dirs)))
+        for j, file in enumerate(files):
+            origin_file_path = os.path.join(path, file)
+            if j < 700:
+                file_path = os.path.join(list_train_dirs[i], file)
+            elif j < 850:
+                file_path = os.path.join(list_validation_dirs[i], file)
+            else:
+                file_path = os.path.join(list_test_dirs[i], file)
+
+            shutil.copyfile(origin_file_path, file_path)
+    print("finish")
+
+
+if __name__ == "__main__":
+    main()
