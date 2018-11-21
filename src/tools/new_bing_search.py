a297308df3b36cc1d158eae66a215652898df307
Łukasz Leszczyński <luki885@gmail.com>
2018-11-21 13:57:17 +0100

    final_web_search

--- src/tools/new_bing_search.py
+++ src/tools/new_bing_search.py
@@ -1,64 +1,78 @@
 from requests import exceptions
-import argparse
 import requests
-#import cv2
-import os
+import google_img
+from threading import Thread
+from to_png import to_jpeg
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-q", "--query", required=True,
-	help="search query to search Bing Image API for")
-ap.add_argument("-o", "--output", required=True,
-	help="path to output directory of images")
-args = vars(ap.parse_args())
+API_KEY = "33e6e287bbb143818c1898325cb471f4"
 
-# API key
-API_KEY = "b79f63162e4a435ca75d35a451eacfff"
-MAX_RESULTS = 5000
-GROUP_SIZE = 50
 
-URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+def get_bing_urls(term, api_key):
+	MAX_RESULTS = 5000
+	GROUP_SIZE = 50
 
-EXCEPTIONS = set([IOError, FileNotFoundError,
-	exceptions.RequestException, exceptions.HTTPError,
-	exceptions.ConnectionError, exceptions.Timeout])
+	URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
 
-term = args["query"]
-headers = {"Ocp-Apim-Subscription-Key" : API_KEY}
-params = {"q": term, "offset": 0, "count": GROUP_SIZE}
+	EXCEPTIONS = {
+		IOError, FileNotFoundError,
+		exceptions.RequestException,
+		exceptions.HTTPError,
+		exceptions.ConnectionError,
+		exceptions.Timeout
+	}
 
-# make the search
-print("[INFO] searching Bing API for '{}'".format(term))
-search = requests.get(URL, headers=headers, params=params)
-search.raise_for_status()
+	headers = {"Ocp-Apim-Subscription-Key": api_key}
+	params = {"q": term, "offset": 0, "count": GROUP_SIZE}
 
-# get the results
-results = search.json()
-estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
-print("[INFO] {} total results for '{}'".format(estNumResults,
-	term))
-
-total = 0
-
-for offset in range(0, estNumResults, GROUP_SIZE):
-	params["offset"] = offset
+	# make the search
 	search = requests.get(URL, headers=headers, params=params)
 	search.raise_for_status()
+
+	# get the results
 	results = search.json()
+	estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
 
-	for v in results["value"]:
-		try:
-			r = requests.get(v["contentUrl"], timeout=30)
+	total = 0
+	list_of_urls = []
 
-			ext = v["contentUrl"][v["contentUrl"].rfind("."):]
-			p = os.path.sep.join([args["output"], "{}{}".format(
-				str(total).zfill(8), ext)])
+	for offset in range(0, estNumResults, GROUP_SIZE):
+		params["offset"] = offset
+		search = requests.get(URL, headers=headers, params=params)
+		search.raise_for_status()
+		results = search.json()
+		for v in results["value"]:
+			try:
+				element = v["contentUrl"], v["contentUrl"][v["contentUrl"].rfind("."):]
+				list_of_urls.append(element)
+			except Exception as e:
+				if type(e) in EXCEPTIONS:
+					print("[INFO] skipping: {}".format(v["contentUrl"]))
+					continue
+			total += 1
 
-			f = open(p, "wb")
-			f.write(r.content)
-			f.close()
+	return list_of_urls
 
-		except Exception as e:
-			if type(e) in EXCEPTIONS:
-				print("[INFO] skipping: {}".format(v["contentUrl"]))
-				continue
-		total += 1
+
+def scrape_bing_img(term):
+	list_of_urls = get_bing_urls(term, API_KEY)
+	google_img.download_imgs(term, list_of_urls)
+
+
+def download_imgs(list_of_terms):
+	print("start")
+	for term in list_of_terms:
+		thread_bing = Thread(target=scrape_bing_img, args=(term,))
+		thread_bing.start()
+		thread_google = Thread(target=google_img.scrape_google_img, args=(term,))
+		thread_google.start()
+
+		thread_bing.join()
+		thread_google.join()
+
+	print("end")
+
+
+list_of_dishes = ['flaczki']
+download_imgs(list_of_dishes)
+for dir in list_of_dishes:
+	to_jpeg(dir)
