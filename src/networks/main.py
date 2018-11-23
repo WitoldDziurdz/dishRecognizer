aa3d5855fe2abec302496f31a48195d0c28bf631
Łukasz Leszczyński <luki885@gmail.com>
2018-11-24 00:43:09 +0100

    added empty VGG16

--- src/networks/main.py
+++ src/networks/main.py
@@ -1,14 +1,23 @@
 from network_tools import utils as u
-from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.callbacks import CSVLogger, ModelCheckpoint, LearningRateScheduler
 from network_tools.data import DataGenerator
-from network_tools.networks import NetworkVGG16, NetworkXception
+from network_tools.networks import NetworkVGG16, NetworkXception, NetworkVGGFromScratch
 from network_tools.architecture import Architecture
 import network_tools.settings as settings
 
+def schedule(epoch):
+    if epoch < 5:
+        return 0.01
+    if epoch < 10:
+        return 0.02
+    else:
+        return 0.004
+
+
 def main():
     # directories
-    base_dir = 'C:\\data\\101food'
-    #base_dir = 'data'
+    #base_dir = 'C:\\data\\101food'
+    base_dir = 'data'
     network_name = 'tmp_name'
     model_path = 'models/' + network_name + '/'
     path_log = model_path + "log.csv"
@@ -18,17 +27,18 @@ def main():
 
     # data generate
     data = DataGenerator(base_dir, settings.input_x, settings.input_y)
-    model = NetworkXception(settings.input_x, settings.input_y, settings.n_classes).create_model()
+    model = NetworkVGGFromScratch(settings.input_x, settings.input_y, settings.n_classes).create_model()
     Architecture(model).log(model_path)
 
     csv_logger = CSVLogger(path_log, append=True, separator=';')
     checkpointer = ModelCheckpoint(filepath=model_path + network_name + '.hdf5', verbose=1, save_best_only=True)
+    lr_scheduler = LearningRateScheduler(schedule)
 
     history = model.fit_generator(
          data.train_generator,
          steps_per_epoch=2000,
          epochs=40,
-         callbacks=[csv_logger, checkpointer],
+         callbacks=[lr_scheduler, csv_logger, checkpointer],
          validation_data=data.validation_generator,
          validation_steps=100, workers=16)
 
--- src/networks/network_tools/networks.py
+++ src/networks/network_tools/networks.py
@@ -53,7 +53,34 @@ class NetworkXception(Network):
 
     def create_model(self):
         self.conv_base.trainable = True
-        self.conv_base.summary()
+        set_trainable = False
+        for layer in self.conv_base.layers:
+            if layer.name == 'block2_sepconv1':
+                set_trainable = True
+            if set_trainable:
+                layer.trainable = True
+            else:
+                layer.trainable = False
+
+        self.model.add(self.conv_base)
+        self.model.add(layers.GlobalAveragePooling2D())
+        self.model.add(layers.Dense(self.n_classes, activation='softmax'))
+        self.model.compile(loss=self.loss, optimizer=self.optimizer, metrics=self.metrics)
+        return self.model
+
+class NetworkVGGFromScratch(Network):
+    def __init__(self, input_x, input_y, n_classes):
+        Network.__init__(self, input_x, input_y, n_classes)
+        self.weights = None
+        self.include_top = False
+        self.conv_base = VGG16(weights=self.weights, include_top=self.include_top, input_shape=self.input_shape)
+        self.loss = 'categorical_crossentropy'
+        self.optimizer = optimizers.SGD(lr=.01, momentum=.9)
+        self.metrics = ['acc']
+        self.model = models.Sequential()
+
+    def create_model(self):
+        self.conv_base.trainable = True
         self.model.add(self.conv_base)
         self.model.add(layers.GlobalAveragePooling2D())
         self.model.add(layers.Dense(self.n_classes, activation='softmax'))
