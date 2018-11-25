182350e80b392222f4f00840548057768a7bb828
Witold.Dzyurdz <witolddzyurdz@gmail.com>
2018-11-25 19:36:01 +0300

    fix bug

--- src/networks/network_tools/networks.py
+++ src/networks/network_tools/networks.py
@@ -9,23 +9,23 @@ class Network:
         self.__input_x = setting.input_x
         self.__input_y = setting.input_y
         self.__input_z = setting.input_z
-        self.__n_classes = setting.n_classes
-        self.__input_shape = (self.__input_x, self.__input_y, self.__input_z)
-        self.__model = models.Sequential()
-        self.__data = data
+        self._n_classes = setting.n_classes
+        self._input_shape = (self.__input_x, self.__input_y, self.__input_z)
+        self._model = models.Sequential()
+        self._data = data
 
     def fit(self, epochs, callbacks):
-        history = self.__model.fit_generator(
-            self.__data.train_data(),
-            steps_per_epoch=700 * self.__n_classes // self.__data.batch_size(),
+        history = self._model.fit_generator(
+            self._data.train_data(),
+            steps_per_epoch=700 * self._n_classes // self._data.batch_size(),
             epochs=epochs,
             callbacks=callbacks,
-            validation_data=self.__data.validate_data(),
-            validation_steps=150 * self.__n_classes // self.__data.batch_size(), workers=16)
+            validation_data=self._data.validate_data(),
+            validation_steps=150 * self._n_classes // self._data.batch_size(), workers=16)
         return history
 
     def evaluate(self, steps):
-        return self.__model.evaluate_generator(self.__data.test_data(), steps)
+        return self._model.evaluate_generator(self._data.test_data(), steps)
 
 
 class NetworkVGG16(Network):
@@ -33,11 +33,10 @@ class NetworkVGG16(Network):
         Network.__init__(self, setting, data)
         self.__weights = 'imagenet'
         self.__include_top = False
-        self.__conv_base = VGG16(weights=self.__weights, include_top=self.__include_top, input_shape=self.__input_shape)
+        self.__conv_base = VGG16(weights=self.__weights, include_top=self.__include_top, input_shape=self._input_shape)
         self.__loss = 'categorical_crossentropy'
         self.__optimizer = optimizers.RMSprop(lr=1e-5)
         self.__metrics = ['acc']
-        self.__model = models.Sequential()
 
     def create_model(self):
         self.__conv_base.trainable = True
@@ -49,12 +48,12 @@ class NetworkVGG16(Network):
                 layer.trainable = True
             else:
                 layer.trainable = False
-        self.__model.add(self.__conv_base)
-        self.__model.add(layers.Flatten())
-        self.__model.add(layers.Dense(4096, activation='relu'))
-        self.__model.add(layers.Dense(self.__n_classes, activation='softmax'))
-        self.__model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
-        return self.__model
+        self._model.add(self.__conv_base)
+        self._model.add(layers.Flatten())
+        self._model.add(layers.Dense(4096, activation='relu'))
+        self._model.add(layers.Dense(self._n_classes, activation='softmax'))
+        self._model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
+        return self._model
 
 
 class NetworkXception(Network):
@@ -62,20 +61,19 @@ class NetworkXception(Network):
         Network.__init__(self, setting, data)
         self.__weights = 'imagenet'
         self.__include_top = False
-        self.__conv_base = Xception(weights=self.__weights, include_top=self.__include_top, input_shape=self.__input_shape)
+        self.__conv_base = Xception(weights=self.__weights, include_top=self.__include_top, input_shape=self._input_shape)
         self.__loss = 'categorical_crossentropy'
         self.__optimizer = optimizers.SGD(lr=.01, momentum=.9)
         self.__metrics = ['acc']
-        self.__model = models.Sequential()
 
     def create_model(self):
         self.__conv_base.trainable = True
-        self.__model.add(self.__conv_base)
-        self.__model.add(layers.GlobalAveragePooling2D())
-        self.__model.add(layers.Dense(1024, activation='relu'))
-        self.__model.add(layers.Dense(self.__n_classes, activation='softmax'))
-        self.__model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
-        return self.__model
+        self._model.add(self.__conv_base)
+        self._model.add(layers.GlobalAveragePooling2D())
+        self._model.add(layers.Dense(1024, activation='relu'))
+        self._model.add(layers.Dense(self._n_classes, activation='softmax'))
+        self._model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
+        return self._model
 
 
 class NetworkVGGFromScratch(Network):
@@ -83,16 +81,15 @@ class NetworkVGGFromScratch(Network):
         Network.__init__(self, setting, data)
         self.__weights = None
         self.__include_top = False
-        self.__conv_base = VGG16(weights=self.__weights, include_top=self.__include_top, input_shape=self.__input_shape)
+        self.__conv_base = VGG16(weights=self.__weights, include_top=self.__include_top, input_shape=self._input_shape)
         self.__loss = 'categorical_crossentropy'
         self.__optimizer = optimizers.SGD(lr=.01, momentum=.9)
         self.__metrics = ['acc']
-        self.__model = models.Sequential()
 
     def create_model(self):
         self.__conv_base.trainable = True
-        self.__model.add(self.__conv_base)
-        self.__model.add(layers.GlobalAveragePooling2D())
-        self.__model.add(layers.Dense(self.__n_classes, activation='softmax'))
-        self.__model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
-        return self.__model
+        self._model.add(self.__conv_base)
+        self._model.add(layers.GlobalAveragePooling2D())
+        self._model.add(layers.Dense(self._n_classes, activation='softmax'))
+        self._model.compile(loss=self.__loss, optimizer=self.__optimizer, metrics=self.__metrics)
+        return self._model
