6b5ca29addeb695dc503faf2ea228258fd5df4a9
Witold.Dzyurdz <witolddzyurdz@gmail.com>
2018-11-25 15:59:37 +0300

    small refactoring nr3

--- src/networks/main.py
+++ src/networks/main.py
@@ -1,9 +1,9 @@
-from network_tools import utils as u
+from network_tools.utils import Util
 from keras.callbacks import CSVLogger, ModelCheckpoint, LearningRateScheduler
 from network_tools.data import DataGenerator
 from network_tools.networks import NetworkVGG16, NetworkXception, NetworkVGGFromScratch
 from network_tools.architecture import Architecture
-import network_tools.settings as settings
+from network_tools.settings import Setting
 
 def schedule(epoch):
     if epoch < 5:
@@ -16,18 +16,19 @@ def schedule(epoch):
 
 def main():
     # directories
-    #base_dir = 'C:\\data\\101food'
-    base_dir = 'data121'
+    base_dir = 'C:\\data\\101food'
+    #base_dir = 'data121'
     network_name = 'tmp_name'
     model_path = 'models/' + network_name + '/'
     path_log = model_path + "log.csv"
     path_name = "."
 
-    u.create_dir(model_path)
-
+    util = Util()
+    util.create_dir(model_path)
+    setting = Setting()
     # data generate
-    data = DataGenerator(base_dir, settings.input_x, settings.input_y)
-    model = NetworkXception(settings.input_x, settings.input_y, settings.n_classes).create_model()
+    data = DataGenerator(base_dir, setting)
+    model = NetworkXception(setting).create_model()
     Architecture(model).log(model_path)
 
     csv_logger = CSVLogger(path_log, append=True, separator=';')
@@ -36,20 +37,20 @@ def main():
 
     history = model.fit_generator(
          data.train_generator,
-         steps_per_epoch=700*settings.n_classes // data.batch_size,
+         steps_per_epoch=700*setting.n_classes // data.batch_size,
          epochs=10,
          callbacks=[lr_scheduler, csv_logger, checkpointer],
          validation_data=data.validation_generator,
-         validation_steps=150*settings.n_classes // data.batch_size, workers=16)
+         validation_steps=150*setting.n_classes // data.batch_size, workers=16)
 
     test_loss, test_acc = model.evaluate_generator(data.test_generator, steps=600)
     print('test acc:', test_acc)
 
     # save model, please check file name
-    u.save_model(model, path_name, test_acc)
+    util.save_model(model, path_name, test_acc)
 
     # loss and accuracy visualization
-    u.visualization_loss_and_accuracy(history=history)
+    util.visualization_loss_and_accuracy(history=history)
 
 
 if __name__ == "__main__":
