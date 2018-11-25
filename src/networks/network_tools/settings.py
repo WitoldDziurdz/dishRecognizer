79a6ebcc16146f90894ee9697c6a74d6985d9721
Witold.Dzyurdz <witolddzyurdz@gmail.com>
2018-11-25 17:47:53 +0300

    change settings

--- src/networks/network_tools/settings.py
+++ src/networks/network_tools/settings.py
@@ -2,7 +2,7 @@
 
 class Setting:
     def __init__(self):
-        self.input_x = 150
-        self.input_y = 150
+        self.input_x = 256
+        self.input_y = 256
         self.input_z = 3
-        self.n_classes = 101
+        self.n_classes = 121
