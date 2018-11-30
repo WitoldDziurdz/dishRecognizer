31f776820bdaa97432968fbd25f41eec3c16fcdd
Łukasz Leszczyński <luki885@gmail.com>
2018-11-30 02:18:10 +0100

    added inception, resnetinception, moved batch size to Setting

--- src/networks/network_tools/settings.py
+++ src/networks/network_tools/settings.py
@@ -2,7 +2,8 @@
 
 class Setting:
     def __init__(self):
-        self.input_x = 256
-        self.input_y = 256
+        self.input_x = 150
+        self.input_y = 150
         self.input_z = 3
-        self.n_classes = 121
+        self.n_classes = 101
+        self.batch_size = 20
